6acc673216e9fa1a1728df0b463a17c169d393e4
Camden_Shaw <camden.shaw@gmail.com>
2017-08-16 17:03:26 -0700

    added adventures photos both to site and to assets folder.  Created empty divs to add photos to later.

--- themes/inhabitant/front-page.php
+++ themes/inhabitant/front-page.php
@@ -17,12 +17,24 @@ get_header(); ?>
 				</header>
 			</article>
 
+			<div class "shop-stuff">
+				<?php
+					$terms = get_terms( 'product-type' );
+						if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+							echo '<ul>';
+							foreach ( $terms as $term ) {
+									echo '<li> <a href="' . get_term_link( $term ) . '">' . $term->name . '</a><p>' . $term->description . '</p></li>';
+							}
+							echo '</ul>';
+					} ?>
+
 			<div class "most-recent-journal">
 				<?php ?>
 				<?php 
 					$args = array( 'post_type' => 'post', 'order' => 'DESC', 'posts_per_page' => '3', 'orderby' => 'date' );
 					$journal_posts = get_posts( $args );
 				?>
+
 				<?php foreach ( $journal_posts as $post ) : setup_postdata( $post ); ?>
 				<div class='journal-recent-block-item'>
 					<div class='journal-thumbnail-wrapper'>
@@ -42,6 +54,34 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+
+	<div class='adventures-container'>
+				<div class='adventures-img1-container'>
+					<div class='adventures-img1'>
+						<h3 class='nature'>Getting Back to Nature in a Canoe</h3>
+						<a href=''>Read More</a>
+					</div>
+				</div>
+				<div class='adventures-img2-container'>
+					<div class='adventures-img2'>
+						<h3 class='beach-friends'>A Night with Friends at the Beach</h3>
+						<a href=''>Read More</a>
+					</div>
+				</div>
+				<div class='adventures-img3-container'>
+					<div class='adventures-img3'>
+						<h3 class='mountain-view'>Taking in the View at Big Mountain</h3>
+						<a href=''>Read More</a>
+					</div>
+				</div>
+				<div class='adventures-img4-container'>
+					<div class='adventures-img4'>
+						<h3 class='stars'>Star-Gazing at the Night Sky</h3>
+						<a href=''>Read More</a>
+					</div>
+				</div>
+				<a href=''>More Adventures</a>
+			</div>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- themes/inhabitant/template-parts/content.php
+++ themes/inhabitant/template-parts/content.php
@@ -8,9 +8,16 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
+	<?php if ( is_page_template( 'archive-product.php') ) : ?>
+		<header class="entry-header">
+			<?php if ( has_post_thumbnail() ) : ?>
+				<?php the_post_thumbnail( 'small' ); ?>
+		<?php endif;	?>
+		<header class="entry-header">
+		<?php else : ?>
+			<?php if ( has_post_thumbnail() ) : ?>
+				<?php the_post_thumbnail( 'large' ); ?>
+			<?php endif; ?>
 		<?php endif; ?>
 
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
