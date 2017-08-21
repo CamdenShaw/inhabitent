f69a7ef4cb8351257f331a27894ccd33d8bb7ea2
Camden_Shaw <camden.shaw@gmail.com>
2017-08-21 04:48:48 -0700

    some basic styling of 404 and search pages

--- themes/inhabitant/page.php
+++ themes/inhabitant/page.php
@@ -6,9 +6,11 @@
  */
 
 get_header(); ?>
+<?php get_sidebar(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="primary-home" class="content-area">
+		<main id="main-home" class="site-main-home" role="main">
+			
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
@@ -19,5 +21,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
