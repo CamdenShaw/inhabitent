d0b170a5808c1535f6ba879e07bfacc76aee7704
Camden_Shaw <camden.shaw@gmail.com>
2017-08-28 15:01:35 -0700

    corrected spacing on the shops page.

--- themes/inhabitant/page-templates/about.php
+++ themes/inhabitant/page-templates/about.php
@@ -7,14 +7,15 @@
 get_header();
  ?>
 <div id="primary" class="content-area">
-  <main id="main" class="site-main" role="main">
 
-    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-      <header class="entry-header custom-hero">
-        <div class="container">
-          <?php the_title( "<h1 class='entry-title'>", "</h1>" ); ?>
-        </div>
-      </header>
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header custom-hero">
+      <div class="container">
+        <?php the_title( "<h1 class='entry-title'>", "</h1>" ); ?>
+      </div>
+    </header>
+
+  <main id="main" class="site-main" role="main">
 
       <div class="container">
         <div class="entry-content">
